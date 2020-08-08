f810a41959d42ce2a1873a76ad68a1c359d5a94b
hgode <hjgode@gmail.com>
2020-08-08 22:03:40 +0200

    adding preferences and settings

---
+++ app/src/main/java/com/sample/batterymonmqtt/MySharedPreferences.java
@@ -0,0 +1,52 @@
+package com.sample.batterymonmqtt;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.preference.PreferenceManager;
+
+public class MySharedPreferences {
+    Context _context=null;
+    SharedPreferences sharedPreferences=null;
+    String mqtt_host="192.168.0.40";
+    String mqtt_interval="30";
+    int mqttInterval=30;
+
+    public MySharedPreferences(Context context){
+        _context=context;
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        mqtt_host=getHost();
+        mqttInterval=getMqqttInterval();
+    }
+
+    public void saveHost(String h){
+        mqtt_host=h;
+        sharedPreferences.edit().putString("mqtt_host", mqtt_host);
+    }
+
+    public void saveInterval(int v){
+        mqtt_interval=v+"";
+        sharedPreferences.edit().putString("mqtt_interval", mqtt_interval);
+    }
+
+    public String getHost(){
+        String h=sharedPreferences.getString("mqtt_host", "192.168.0.40");
+        return h;
+    }
+
+    public int getMqqttInterval(){
+        int v=30;
+        mqtt_interval=sharedPreferences.getString("mqtt_interval", "30");
+        try{
+            v=Integer.parseInt(mqtt_interval);
+        }catch(Exception ex){
+            v=30;
+            Log.d(MainActivity.TAG, "SharedPreferneces, mqtt_interval not a number?: "+ex.getMessage());
+        }
+        return v;
+    }
+    public String getMqttHost(){
+        return mqtt_host;
+    }
+}
---
+++ app/src/main/java/com/sample/batterymonmqtt/SettingsActivity.java
@@ -0,0 +1,86 @@
+package com.sample.batterymonmqtt;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat implements
+            SharedPreferences.OnSharedPreferenceChangeListener {
+        static String TAG="SettingsFragment";
+
+        @Override
+        public void onResume(){
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+        @Override
+        public void onPause(){
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            Log.d(TAG, "SettingsFragment onCreatePreferences");
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+            PreferenceScreen preferenceScreen = getPreferenceScreen();
+            androidx.preference.EditTextPreference editTextPreference = getPreferenceManager().findPreference("use_key_from_editTextPreference_in_xml_file");
+            editTextPreference.setOnBindEditTextListener(new androidx.preference.EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+
+                }
+            });
+        }
+
+        public static SettingsFragment newInstance() {
+            Log.d(TAG, "SettingsFragment newInstance");
+            SettingsFragment fragment = new SettingsFragment();
+            return fragment;
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Log.d(TAG, "SettingsFragment onSharedPreferenceChanged called with key="+key);
+            Preference preference = findPreference(key);
+
+            if (preference != null) {
+
+                if (!(preference instanceof CheckBoxPreference)) {
+                    String value = sharedPreferences.getString(preference.getKey(), "");
+                    Log.i(TAG, "changed key/value: " + key+"/"+value);
+                }
+            }
+
+        }
+
+    }
+}
