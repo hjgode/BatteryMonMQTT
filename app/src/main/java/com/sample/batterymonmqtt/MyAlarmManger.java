e6b77ac3935c31be10d7d6ff13111bb2d69bb3a9
hgode <hjgode@gmail.com>
2020-08-20 06:16:21 +0200

    trying to get a reliable periodic update with AlarmManager

---
+++ app/src/main/java/com/sample/batterymonmqtt/MyAlarmManger.java
@@ -0,0 +1,49 @@
+package com.sample.batterymonmqtt;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+
+public class MyAlarmManger {
+    Context context=null;
+    final int requestId=815;
+    PendingIntent alarmIntent=null;
+    Intent intentAlarmReceiver = new Intent(context, MyAlarmReceiver.class);
+
+    static AlarmManager alarmManager=null;
+
+    public MyAlarmManger(Context _context){
+        context= _context;
+        if(alarmManager==null)
+            alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intentAlarmReceiver, 0);
+
+    }
+
+    void scheduleWakeup(int interval){
+        //this needs to be rescheduled!
+//        alarmManager.setAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME, 1*60*1000, alarmIntent);
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME, interval*60*1000, alarmIntent);
+/*        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HALF_HOUR,
+                interval*60*1000
+                //AlarmManager.INTERVAL_HALF_HOUR
+                , alarmIntent);
+*/
+    }
+    public void cancelAlarm()
+    {
+//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent =
+                PendingIntent.getService(context, requestId, intentAlarmReceiver,
+                        PendingIntent.FLAG_NO_CREATE); //should be the same as alarmIntent
+        if (pendingIntent != null && alarmManager != null) {
+            alarmManager.cancel(pendingIntent);
+            //alarmManager.cancel(alarmIntent);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/sample/batterymonmqtt/MyAlarmReceiver.java
@@ -0,0 +1,17 @@
+package com.sample.batterymonmqtt;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyAlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Is triggered when alarm goes off, i.e. receiving a system broadcast
+        if(intent.getAction()==""){
+            MQTTPublisher mqttPublisher=new MQTTPublisher();
+            mqttPublisher.doPublish(this, new BatteryInfo.BattInfo(),);
+        }
+    }
+}
